7e4a39c1bdb050f1856c8a721afe864587791f1c
Victor Manuel Yanez Aguilar <victor.yanez@VMT.LOCAL>
2023-02-10 16:17:06 -0600

    Pruebas de coneccion y captura textos

---
+++ ControlSuministrosImpresor/Controllers/InstitucionesController.cs
@@ -0,0 +1,50 @@
+﻿using ControlSuministrosImpresor.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ControlSuministrosImpresor.Controllers
+{
+    public class InstitucionesController : Controller
+    {
+
+/*
+
+        private readonly string connectionString;
+
+        public InstitucionesController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+*/
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear( Institucion institucion)
+        {
+            return View();
+        }
+
+        /*
+                public IActionResult Crear()
+                {
+                    using (var connection = new SqlConnection(connectionString))
+
+                    {
+
+                        var query = connection.Query("SELECT 1").FirstOrDefault();
+
+                    }
+                    return View();
+
+                }
+
+         */
+
+    }
+}
+
